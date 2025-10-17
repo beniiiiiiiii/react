15c71e225ce034809e3a539710b7d607d3b4328d
Csombo-Varga Benedek <b1csoben@vasvari.org>
2025-10-17 10:41:31 +0200

    post, put

---
+++ 1017/src/main.tsx
@@ -0,0 +1,16 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import NewPizza from "./pages/NewPizza.tsx";
+import EditPizza from "./pages/EditPizza.tsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/edit-pizza/:id" element={<EditPizza />} />
+        <Route path="/new-pizza" element={<NewPizza />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
