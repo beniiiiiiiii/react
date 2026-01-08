e73b8481304511e1a2229e2cd901f62e9ced6459
binaj <b1csoben@vasvari.org>
2026-01-08 12:29:53 +0100

    basic auth

--- felevelenites/src/main.tsx
+++ felevelenites/src/main.tsx
@@ -9,12 +9,16 @@ import EditPizza from "./pages/EditPizza.tsx";
 import NewPizza from "./pages/NewPizza.tsx";
 import NotFoundPage from "./pages/NotFoundPage.tsx";
 import Kosar from "./pages/Kosar.tsx";
+import Login from "./pages/Logn.tsx";
+import Orders from "./pages/Orders.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<AllPizza />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/orders" element={<Orders />} />
         <Route path="pizza/:id" element={<OnePizza />} />
         <Route path="/edit-pizza/:id" element={<EditPizza />} />
         <Route path="/new-pizza" element={<NewPizza />} />
